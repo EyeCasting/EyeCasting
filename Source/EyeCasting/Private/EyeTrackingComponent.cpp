2afb6e0e8ce030b636203196179b3b0d171ac9fe
Nathan <natecas08@gmail.com>
2023-12-08 00:05:14 -0800

    Set up eye tracking function calls

--- Source/EyeCasting/Private/EyeTrackingComponent.cpp
+++ Source/EyeCasting/Private/EyeTrackingComponent.cpp
@@ -1,16 +1,38 @@
 // EyeTrackingComponent.cpp
 
 #include "EyeTrackingComponent.h"
+#include "OculusXREyeTrackingComponent.h"
 
 UEyeTrackingComponent::UEyeTrackingComponent()
 {
     PrimaryComponentTick.bCanEverTick = true;
+
+	eyeTracker.BeginPlay();
+	eyeTracker.ClearRotationValues();
 }
 
 FVector UEyeTrackingComponent::GetGazeDirection() const
 {
     // This will eventually interface with the actual eye tracking hardware/software
     // and return the current gaze direction of the user
+
+
     return CurrentGazeDirection;
+}
+
+void UEyeTrackingComponent::updateGazeData() const
+{
+	eyeTracker.bUpdatePosition = true;
+	eyeTracker.bUpdateRotation = true;
+}
+
+void UEyeTrackingComponent::drawGazeLocation() const
+{
+	//code that draws the gaze location
+}
+
+FVector UEyeTrackingComponent::getRelativeGazeLocation() const
+{
+
 }
 
--- Source/EyeCasting/Public/EyeTrackingComponent.h
+++ Source/EyeCasting/Public/EyeTrackingComponent.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "EyeTrackingComponent.generated.h"
+#include "OculusXREyeTrackingComponent.h"
 
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class EYECasting_API UEyeTrackingComponent : public UActorComponent
@@ -12,12 +13,18 @@ class EYECasting_API UEyeTrackingComponent : public UActorComponent
     GENERATED_BODY()
 
 public:
+	UOculusXREyeTrackingComponent eyeTracker;
+
     UEyeTrackingComponent();
 
     UFUNCTION(BlueprintCallable, Category = "EyeTracking")
     FVector GetGazeDirection() const;
+	void drawGazeLocation() const;
+	void updateGazeData() const;
 
 protected:
+	FVector getRelativeGazeLocation() const;
+
     UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "EyeTracking")
     FVector CurrentGazeDirection;
 };
