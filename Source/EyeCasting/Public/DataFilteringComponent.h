ca4b0b4725cac8ac5be3782dfd0b49a03a29a789
Luna Perez-Herrera <89598407+Perez-Herrera-Luna@users.noreply.github.com>
2023-12-17 17:47:15 -0800

    New data filtering component implementation
    Moved to correct directory, old LunaDev branch deleted to adhere to naming convention

--- Source/EyeCasting/Public/DataFilteringComponent.h
+++ Source/EyeCasting/Public/DataFilteringComponent.h
@@ -1,1 +1,32 @@
-//needs implementation
+#include "CoreMinimal.h"
+#include "Components/SceneCaptureComponent2D.h"
+#include "FishEyeViewComponent.generated.h"
+#include <iterator>
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class DataFilteringComponent : public USceneCaptureComponent2D
+{
+    GENERATED_BODY()
+
+public:
+    FEyeTrackerGazeData[] gazeDataArray;
+    FString filteringMode;
+
+    UDataFilteringComponent();
+    UDataFilteringComponent(const FObjectInitializer &ObjectInitializer);
+    ~UDataFilteringComponent();
+
+    void setGazeDataArray(FEyeTrackerGazeData[] gazeDataArray);
+    bool setFilteringMode(FString filteringMode);
+
+    FVector getFilteredGazeVector();
+
+private:
+    FVector filteredGazeVector;
+
+    FVector averageLastXVectors(const FVector[] vector);
+    FVector medianLastXVectors(const FVector[] vector);
+    FVector excludeOutlier(const FVector[] vector);
+    FVector excludeMostRecent(const FVector[] vector);
+    void clearGazeDataArray();
+};
